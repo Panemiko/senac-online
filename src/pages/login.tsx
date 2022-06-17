268838ca27ff4f4c1354eaa2e692ace5c2316c5a
Panem <titoluccao@gmail.com>
2022-06-17 13:01:00 -0300

    feat(pages): add inputs, button and a to login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { styled } from '@styles/stitches.config'
 import Page from '@components/Page'
 import Heading from '@components/Heading'
+import TextInput from '@components/TextInput'
+import Link from '@components/Link'
+import Button from '@components/Button'
 
 const Container = styled('main', {
     height: '100vh',
@@ -11,9 +14,10 @@ const Container = styled('main', {
 })
 
 const FormContainer = styled('div', {
+    width: '420px',
     paddingY: '$11',
     paddingX: '$8',
-    borderRadius: '$4 0 0 $4',
+    borderRadius: '$4',
     background: '$backgroundMedium',
     textAlign: 'center',
 })
@@ -26,7 +30,24 @@ export default function Register() {
                     <Heading as={'h2' as any} variant='tertiary'>
                         Entrar
                     </Heading>
-                    <Heading variant='primary'>Senac Online</Heading>
+                    <Heading css={{ marginBottom: '$8' }} variant='primary'>
+                        Senac Online
+                    </Heading>
+                    <TextInput
+                        css={{ marginBottom: '$7' }}
+                        type='email'
+                        autoComplete='off'
+                        placeholder='E-mail'
+                    />
+                    <TextInput
+                        css={{ marginBottom: '$8' }}
+                        type='password'
+                        autoComplete='off'
+                        placeholder='Senha'
+                    />
+                    <Link href='#'>Esqueceu sua senha?</Link>
+                    <br />
+                    <Button css={{ marginTop: '$7' }}>Entrar</Button>
                 </FormContainer>
             </Container>
         </Page>

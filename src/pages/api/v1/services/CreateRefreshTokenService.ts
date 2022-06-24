f5fb0a29a4e5e2d59e70e38ff4165fd067f9aeba
Panem <titoluccao@gmail.com>
2022-06-24 15:07:32 -0300

    feat(services): add CreateRefreshTokenService

---
+++ src/pages/api/v1/services/CreateRefreshTokenService.ts
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken'
+import { RefreshTokenRepository } from '../repositories'
+import Settings from '../Settings'
+import Service from './Service'
+
+export default class CreateRefreshTokenService extends Service {
+    async execute(userId: string) {
+        const refreshTokenRepository = new RefreshTokenRepository()
+        const settings = new Settings()
+
+        const secret = await settings.getRefreshTokenSecret()
+        const token = jwt.sign(userId, secret || 'token', { expiresIn: '30d' })
+
+        return await refreshTokenRepository.registerRefreshToken(userId, token)
+    }
+}
--- src/pages/api/v1/services/index.ts
+++ src/pages/api/v1/services/index.ts
@@ -1,1 +1,4 @@
 export { default as SanitizeUserService } from './SanitizeUserService'
+export { default as EncryptPasswordService } from './EncryptPasswordService'
+export { default as CompareEncryptedPasswordService } from './CompareEncryptedPasswordService'
+export { default as CreateRefreshTokenService } from './CreateRefreshTokenService'
